a17784c11e2be9eb8bfe8b0231f6c76c037d793e
Drone181 <89610668+Drone181@users.noreply.github.com>
2024-10-26 20:43:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -124,5 +124,6 @@ def after_request(response):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
